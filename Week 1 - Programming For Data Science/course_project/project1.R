80a4639d73c6f4c495b0a04ebcf4ea0415480e0f
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-01-20 12:35:13 +0700

    Finish Algoritma Learn By Building project 1 - Programming for Data Science

--- Week 1 - Programming For Data Science/course_project/project1.R
+++ Week 1 - Programming For Data Science/course_project/project1.R
@@ -1,65 +1,59 @@
-# Setting working directory as a code starter
+# Algoritma Learn by Building
+# R Programming for Data Science
+
+# Project 1
+# R Script and transform the data:
+#   Write a R script containing a function (name the function however way you want)
+#   This function is for getting information/insight from your data.
+#   This function can contain several things as the base requirement but free to customize your script to add any extra functionalities,
+#   Reads retail.csv file
+#   Perform transform/pre-processing data (as.factor, as.Date, subsetting, append row/column, etc.)
+#   Perform data manipulation (xtabs or aggregate)
+#   Export cross-tabulation or plot as output (data frame, plot)
+
+# Setting working directory as a project starter
 setwd("Week 1 - Programming For Data Science")
 getwd()
 
-# Load Library
+# I try to define my own question
+# QUESTION: How was our Profit performance in 2017 based on Category and Segment variable?
+
+# Load lubridate library as needed for "DATE" data manipulation
 library(lubridate)
 
-# Create function for getting information/insight from your data
-#getInsight <- function(dataSource = "") {
-  #TODO
-#}
-
-#getInsight(dataSource = "data_input/retail.csv")
-
-
-# Read data using common function read.cs 
+# Read data using common function read.csv()
 retail <- read.csv("data_input/retail.csv")
-
-# Take a peak of our data using head() and tail()
-head(retail)
-tail(retail)
 
 # Try to understand the structure of our data frame
 str(retail)
 summary(retail)
 
-# pre-processing variables which supposed to have wrong class
-retail$Row.ID <- as.character(retail$Row.ID)
+# pre-processing variables which supposed to be used and have wrong class
 retail$Order.Date <- as.Date(retail$Order.Date, "%m/%d/%y")
 retail$Ship.Date <- as.Date(retail$Ship.Date, "%m/%d/%y")
-retail$Customer.ID <- as.character(retail$Customer.ID)
-retail$Product.ID <- as.character(retail$Product.ID)
-retail$Product.Name <- as.character(retail$Product.Name)
-
-# Formulate questions
-# table() used for factor (categorical) variable
-
-# 1. How many our shipments were on Standard Class?
-table(retail$Ship.Mode) # 1st method
-summary(retail) # 2nd method
-# Answer: 5968
-
-# 2. How many Technology product did we sell to Corporate customers?
-table(retail$Segment, retail$Category)
-# Answer: 554
-
-# 3. How many percentage of our sales were shipped to Consumer on Standard Class?
-prop <- prop.table(table(retail$Segment, retail$Ship.Mode)) * 100
-round(prop, digits = 2) # round numeric value to 2 decimal digits
-# Answer: 30.87%
 
 # "Feature Engineering", create new column based on current column
 retail$Order.Year <- year(retail$Order.Date)
-retail$Order.Month <- month(retail$Order.Date)
+
+# Subset data for year 2017 and remove unused column
+r2017 <- retail[retail$Order.Year == 2017, -c(1, 2, 6, 8)]
+summary(r2017)
+
+# Remove unused dataframe in order to save memory
+rm(retail)
 
 # Use xtabs for data manipulation
 # xtabs() can be used for numeric depending on categorical variables
 # by default, it uses the sum() as the breakdown
-xtabs(formula = Profit ~ Category + Segment, data = retail)
+xt1 <- xtabs(formula = Profit ~ Category + Segment, data = r2017)
+xt1
+plot(xt1, main = "2017 Profit on Category and Segment")
 
 # Use aggregate for data manipulation
 # very similar to xtabs except it takes an additional parameter
-aggr1 <- aggregate(formula = Profit ~ Category + Segment, data = retail, FUN = mean)
-profit_category_segment <- as.data.frame(aggr1)
-plot(profit_category_segment)
+aggr1 <- aggregate(formula = Profit ~ Category + Segment, data = r2017, FUN = sum)
+profit2017 <- as.data.frame(aggr1)
+print(profit2017)
+
+# Save our processed data into .csv file
+write.csv(profit2017, file = "profit2017.csv")
---
+++ Week 1 - Programming For Data Science/recall_material.R
@@ -0,0 +1,65 @@
+# Setting working directory as a code starter
+setwd("Week 1 - Programming For Data Science")
+getwd()
+
+# Load Library
+library(lubridate)
+
+# Create function for getting information/insight from your data
+#getInsight <- function(dataSource = "") {
+#TODO
+#}
+
+#getInsight(dataSource = "data_input/retail.csv")
+
+
+# Read data using common function read.cs 
+retail <- read.csv("data_input/retail.csv")
+
+# Take a peak of our data using head() and tail()
+head(retail)
+tail(retail)
+
+# Try to understand the structure of our data frame
+str(retail)
+summary(retail)
+
+# pre-processing variables which supposed to have wrong class
+retail$Row.ID <- as.character(retail$Row.ID)
+retail$Order.Date <- as.Date(retail$Order.Date, "%m/%d/%y")
+retail$Ship.Date <- as.Date(retail$Ship.Date, "%m/%d/%y")
+retail$Customer.ID <- as.character(retail$Customer.ID)
+retail$Product.ID <- as.character(retail$Product.ID)
+retail$Product.Name <- as.character(retail$Product.Name)
+
+# Formulate questions
+# table() used for factor (categorical) variable
+
+# 1. How many our shipments were on Standard Class?
+table(retail$Ship.Mode) # 1st method
+summary(retail) # 2nd method
+# Answer: 5968
+
+# 2. How many Technology product did we sell to Corporate customers?
+table(retail$Segment, retail$Category)
+# Answer: 554
+
+# 3. How many percentage of our sales were shipped to Consumer on Standard Class?
+prop <- prop.table(table(retail$Segment, retail$Ship.Mode)) * 100
+round(prop, digits = 2) # round numeric value to 2 decimal digits
+# Answer: 30.87%
+
+# "Feature Engineering", create new column based on current column
+retail$Order.Year <- year(retail$Order.Date)
+retail$Order.Month <- month(retail$Order.Date)
+
+# Use xtabs for data manipulation
+# xtabs() can be used for numeric depending on categorical variables
+# by default, it uses the sum() as the breakdown
+xtabs(formula = Profit ~ Category + Segment, data = retail)
+
+# Use aggregate for data manipulation
+# very similar to xtabs except it takes an additional parameter
+aggr1 <- aggregate(formula = Profit ~ Category + Segment, data = retail, FUN = mean)
+profit_category_segment <- as.data.frame(aggr1)
+plot(profit_category_segment)
