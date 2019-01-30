2f3737da6192055085ada6bf9dc7c66cdf005b76
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-01-30 19:43:41 +0700

    First step learning shiny

--- 04_InteractivePlotting/shiny/Learn_Shiny/app.R
+++ 04_InteractivePlotting/shiny/Learn_Shiny/app.R
@@ -1,14 +1,29 @@
 library(shiny)
+library(ggplot2)
+library(dplyr)
+
+vids <- read.csv("youtubetrends.csv")
 
 ui <- fluidPage(
   sidebarLayout(
-    sidebarPanel(),
-    mainPanel()
+    sidebarPanel(
+      sliderInput(inputId = "sliderID", 
+                  label = "Number of Likes", 
+                  min = 0, 
+                  max = 50, 
+                  value = 25)
+    ),
+    mainPanel(
+      plotOutput(outputId = "plothist")
+      )
   )
 )
 
 server <- function(input, output, session) {
-  
+  output$plothist <- renderPlot({
+    ggplot(data = vids, aes(x = likes)) +
+      geom_histogram(bins = input$sliderID)
+  })
 }
 
 shinyApp(ui, server)
