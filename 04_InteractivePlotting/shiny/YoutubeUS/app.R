ba45b875aa93a7b86a74a205ef8cd75334f34436
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-01-30 20:53:38 +0700

    Finish Shiny Dashboard with 2 menus, 2 inputs, and 2 plots

--- 04_InteractivePlotting/shiny/YoutubeUS/app.R
+++ 04_InteractivePlotting/shiny/YoutubeUS/app.R
@@ -1,12 +1,67 @@
 library(shiny)
 library(shinydashboard)
+library(ggplot2)
+library(dplyr)
+library(plotly)
 
-ui <- dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody()
+vids <- read.csv("youtubetrends.csv")
+
+ui <- dashboardPage(skin = "red",
+  dashboardHeader(title = "Youtube Dashboard"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Category", tabName = "category", icon = icon("dashboard")),
+      menuItem("Publish Hour", tabName = "publishHour", icon = icon("dashboard"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "category",
+              selectInput(inputId = "categoryInput",
+                          label = "Category",
+                          choices = levels(vids$category_id)),
+              plotlyOutput(outputId = "plotcategory")
+              ),
+      
+      tabItem(tabName = "publishHour",
+              sliderInput(inputId = "publishHourInput", 
+                          label = "Choose Publish Hour", 
+                          min = 0, 
+                          max = 23, 
+                          value = 12,
+                          step = 1),
+              plotlyOutput(outputId = "plotHour"))
+    )
+  )
 )
 
-server <- function(input, output) { }
+server <- function(input, output) { 
+  
+  output$plotcategory <- renderPlotly({
+    vids.category <- vids %>% 
+      filter(category_id == input$categoryInput)
+    
+    plot1 <-  ggplot(data = vids.category, aes(x = likes, y = dislikes)) +
+      geom_point()
+    
+    ggplotly(plot1)
+  
+  })
+  
+
+  output$plotHour <- renderPlotly({
+    vids.hour <- vids %>%
+      filter(publish_hour == input$publishHourInput) %>% 
+      group_by(category_id) %>% 
+      summarise(totalVideo = n())
+    
+    plot2 <- ggplot(data = vids.hour, aes(x = reorder(category_id, totalVideo),
+                                          y = totalVideo)) +
+      geom_col() +
+      coord_flip()
+    
+    ggplotly(plot2)
+  })
+}
 
 shinyApp(ui, server)
