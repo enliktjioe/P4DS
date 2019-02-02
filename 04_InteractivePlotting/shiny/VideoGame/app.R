4d9b778f4128ae07e31e8067de31cdbe4a756beb
Enlik Tjioe <enlik.stefanus@gmail.com>
2019-02-02 15:01:35 +0700

    Hide Legend implementation

--- 04_InteractivePlotting/shiny/VideoGame/app.R
+++ 04_InteractivePlotting/shiny/VideoGame/app.R
@@ -117,7 +117,8 @@ server <- function(input, output) {
       theme(axis.text.x = element_text(angle=90, hjust=1)) +
       labs(title = "Review Score", x = "Platform", y = "Total")
     
-    ggplotly(plot3)
+    hide_legend(ggplotly(plot3))
+    
   })
 }
 
